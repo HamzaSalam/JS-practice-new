cd2aac09851e8b1e63a99394e64a2b381cb6f574
HamzaSalam <hamza.salam786@gmail.com>
2024-03-09 19:18:51 +0500

    truthy

--- calculator challenges/script.js
+++ calculator challenges/script.js
@@ -15,8 +15,8 @@ const calculator = (num1,num2,operator) =>{
     return "plase choose correct operator between -,+,*,/"
 }
 
-console.log(calculator(5, 2, '+')) // returns 7
-console.log(calculator(5, 2, '-'))// returns 3
-console.log(calculator(5, 2, '*'))// returns 10
-console.log(calculator(5, 2, '/'))// returns 2.5
-console.log(calculator(5, 2, '&'))// returns an error message
+console.log(calculator(5, 2, '/')) // returns 7
+// console.log(calculator(5, 2, '-'))// returns 3
+// console.log(calculator(5, 2, '*'))// returns 10
+// console.log(calculator(5, 2, '/'))// returns 2.5
+// console.log(calculator(5, 2, '&'))// returns an error message
