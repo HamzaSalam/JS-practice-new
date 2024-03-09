13f290b61cac0ea231a8cb09e6c434c8423823c8
HamzaSalam <hamza.salam786@gmail.com>
2024-03-09 19:02:13 +0500

    switch

---
+++ calculator challenges/script.js
@@ -0,0 +1,22 @@
+const calculator = (num1,num2,operator) =>{
+    if(operator === "+"){
+        return `${num1 + num2}`
+    } 
+    else if(operator === "-"){
+        return num1 - num2 
+    } 
+    else if(operator === "*"){
+        return num1 * num2
+    } 
+    else if(operator === "/"){
+        return num1 / num2
+    }
+    else if(operator === "&") 
+    return "plase choose correct operator between -,+,*,/"
+}
+
+console.log(calculator(5, 2, '+')) // returns 7
+console.log(calculator(5, 2, '-'))// returns 3
+console.log(calculator(5, 2, '*'))// returns 10
+console.log(calculator(5, 2, '/'))// returns 2.5
+console.log(calculator(5, 2, '&'))// returns an error message
